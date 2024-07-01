6c9fe0331484ae7abf7db6d5a706f7456473c4b5
adeyemitobiloba <tobilobaadeyemi400@gmail.com>
2024-07-01 10:02:02 +0100

    student score

---
+++ studentscore.py
@@ -0,0 +1,64 @@
+studentscore=int(input("enter your score:"))
+english=75
+mathematics=80
+chemistry=75
+physics=74
+biology=80
+civic=86
+economics=82
+agric=80
+dataprocessing=85
+marketing=92
+total_subject=75,80,75,74,80,86,82,80,85,92
+if studentscore==total_subject:
+    print("A")
+elif studentscore>physics:
+    print("A")
+elif studentscore<civic:
+    print("B")
+elif studentscore>mathematics:
+    print("A")
+elif studentscore<physics:
+    print("B")
+elif studentscore>biology:
+    print("A")
+elif studentscore<marketing:
+    print("C")
+elif studentscore<dataprocessing:
+    print("B")
+elif studentscore>chemistry:
+    print("A")
+else:
+    print("education should be your priority")    
+studentscore=int(input("enter your score:"))
+english=75
+mathematics=80
+chemistry=75
+physics=74
+biology=80
+civic=86
+economics=82
+agric=80
+dataprocessing=85
+marketing=92
+total_subject=75,80,75,74,80,86,82,80,85,92
+if studentscore==total_subject:
+    print("A")
+elif studentscore>physics:
+    print("A")
+elif studentscore<civic:
+    print("B")
+elif studentscore>mathematics:
+    print("A")
+elif studentscore<physics:
+    print("B")
+elif studentscore>biology:
+    print("A")
+elif studentscore<marketing:
+    print("C")
+elif studentscore<dataprocessing:
+    print("B")
+elif studentscore>chemistry:
+    print("A")
+else:
+    print("education should be your priority")    
